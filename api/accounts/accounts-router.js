db766a47e812eac81ca3e68fb263f82cc46d8731
Tom Bielawski <tfbielawski@protonmail.com>
2021-09-03 21:58:12 -0400

    "routing"

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,45 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+    try{
+        res.json("get accounts")
+    }
+    catch(err){next(err)}
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+    try{
+        res.json("get accounts by id")
+    }
+    catch(err){next(err)}
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+    try{
+        res.json("create accounts")
+    }
+    catch(err){next(err)}
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+    try{
+        res.json("update accounts")
+    }
+    catch(err){next(err)}
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+    try{
+        res.json("delete accounts")
+    }
+    catch(err){next(err)}
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+    try{
+        res.status(err.status || 500).json({message: err.message})
+    }
+    catch(err){next(err)}
 })
 
 module.exports = router;
